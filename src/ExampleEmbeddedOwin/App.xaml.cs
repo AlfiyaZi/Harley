8422a679914f0ff70bc5aed6a080425bdf924181
Damian Hickey <dhickey@gmail.com>
2014-09-23 11:23:09 +0200

    MainWindow ctor overload that takes an AppFunc.

--- src/ExampleEmbeddedOwin/App.xaml.cs
+++ src/ExampleEmbeddedOwin/App.xaml.cs
@@ -1,6 +1,12 @@
 ﻿namespace ExampleEmbeddedOwin
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
     using System.Windows;
+    using Harley.UI;
+    using Microsoft.Owin.Builder;
+    using Owin;
 
     public partial class App : Application
     {
@@ -8,7 +14,11 @@
         {
             base.OnStartup(e);
 
-            new MainWindow().Show();
+            var appBuilder = new AppBuilder();
+            appBuilder.UseWelcomePage();
+            Func<IDictionary<string, object>, Task> appFunc = appBuilder.Build();
+
+            new MainWindow("Owin Embedded Application", appFunc).Show();
         }
     }
 }
--- src/Harley.UI/MainWindow.xaml.cs
+++ src/Harley.UI/MainWindow.xaml.cs
@@ -1,5 +1,11 @@
 ﻿namespace Harley.UI
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using CefSharp;
+    using Harley.UI.Owin;
+
     public partial class MainWindow
     {
         public MainWindow(string title = null, string address = null)
@@ -9,6 +15,21 @@
             EmbeddedBrowser.Init();
             InitializeComponent();
             Browser.Address = address;
+        }
+
+        public MainWindow(string title, Func<IDictionary<string, object>, Task> appFunc)
+        {
+            Title = title ?? Title;
+            EmbeddedBrowser.Init(new[]
+            {
+                new CefCustomScheme
+                {
+                    SchemeHandlerFactory = new OwinSchemeHandlerFactory(appFunc),
+                    SchemeName = "http"
+                }
+            });
+            InitializeComponent();
+            Browser.Address = "http://localhost";
         }
     }
 }
